576f4ab94602eb1696ebd81805f04bf6dfaea490
Steven <sunetwli102@gmail.com>
2018-04-19 19:50:26 -0500

    This is weather api demo 1. used ajax to fetch jason data 2. used promise to handle response 3. Jquery and Bootstrap to handle data and render it

---
+++ wehter_demo/js/main.js
@@ -0,0 +1,60 @@
+function getWeather() {
+    var x = document.getElementById('weather');
+    let locationZipCode = x.value;
+    console.log(locationZipCode);
+    let request = new XMLHttpRequest();
+    return new Promise(function (resolve, reject) {
+        let url = "http://api.wunderground.com/api/6edf9621eab3f362/forecast/q/CA/" + locationZipCode + ".json"
+        request.onreadystatechange = () => {
+            if (request.readyState === 4) {
+                if (request.status === 200) {
+                    resolve(request.response);
+                    // console.log(request.response);
+                }
+                else {
+                    reject(request.status);
+                }
+            }
+        }
+        request.open('GET', url);
+        request.send();
+    });
+}
+
+var clickWeather = function () {
+    getWeather().then((response) => {
+        console.log("YYY is resolving now")
+
+        let high_f = JSON.parse(response)["forecast"]["simpleforecast"]["forecastday"][0]["high"]["fahrenheit"];
+        let high_c = JSON.parse(response)["forecast"]["simpleforecast"]["forecastday"][0]["high"]["celsius"];
+        let low_f = JSON.parse(response)["forecast"]["simpleforecast"]["forecastday"][0]["low"]["fahrenheit"];
+        let low_c = JSON.parse(response)["forecast"]["simpleforecast"]["forecastday"][0]["low"]["celsius"];
+        let condition = JSON.parse(response)["forecast"]["simpleforecast"]["forecastday"][0]["conditions"];
+
+        console.log(high_c + " " + high_f);
+        console.log(low_c + " " + low_f);
+        console.log(condition);
+
+        deleteNodes();
+
+        $('#weather_condition').append(`<p><b>Highest Temperature: </b> ${high_c}<b>C</b> / ${high_f}<b>F</b></p>`);
+        $('#weather_condition').append(`<p><b>Lowest Temperature: </b> ${low_c}<b>C</b> / ${low_f}<b>F</b></p>`);
+        $('#weather_condition').append(`<p><b>Current Conditions: </b> ${condition}`);
+    }).catch((response) => {
+        console.log("YYY is rejecting now");
+        $('#weather_condition').append(`<p>Error: ${response}</p>`);
+    })
+}
+
+
+
+
+
+
+function deleteNodes() {
+    console.log("removing ---");
+    var d = document.getElementById('weather_condition');
+    while (d.firstChild) {
+        d.removeChild(d.firstChild);
+    }
+}
